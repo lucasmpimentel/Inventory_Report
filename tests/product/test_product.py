a7caea285b34d79b4bd041126c9e1f8a38290afb
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-10-16 17:45:21 -0300

    fix: lint error

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -17,4 +17,6 @@ def test_cria_produto():
     assert result.data_de_fabricacao == "2022-01-02"
     assert result.data_de_validade == "2022-12-12"
     assert result.numero_de_serie == "123456"
-    assert result.instrucoes_de_armazenamento == "Isso não existe, é só um teste"
+    assert result.instrucoes_de_armazenamento == (
+        "Isso não existe, é só um teste"
+    )
